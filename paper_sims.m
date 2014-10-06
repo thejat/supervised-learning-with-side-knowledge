4af06fe5311178c1c23866cd460cec72c6376ecd
Theja Tulabandhula <theja@mit.edu>
2014-10-06 10:14:16 +0530

    Par class not working as expected. removing it and other relegated code.

--- paper_sims.m
+++ paper_sims.m
@@ -24,11 +24,9 @@ sampleTestY = sampleTestX*betaTrue + noiseSigma*randn(nTest,1) + noiseSigma*trnd
 sampleKnowledgeX = randn(nKnowledge,nBeta)*chol(featureSigma); %kept same for a given beta
 sampleKnowledgeY = sampleKnowledgeX*betaTrue; %No need to add noise% + noiseSigma*randn(nKnowledge,1);
 
-
-tic
 for i=1:nRuns %Multiple samples from the data source.
 
-    fprintf('Ridge Regression. Run %d of %d\n',i,nRuns);
+    fprintf('Ridge Regression.\n');
     
     sampleTrainX = randn(max(nTrainArray),nBeta)*chol(featureSigma);
     sampleTrainY = sampleTrainX*betaTrue + noiseSigma*randn(max(nTrainArray),1) + noiseSigma*trnd(1,max(nTrainArray),1);
@@ -44,68 +42,98 @@ for i=1:nRuns %Multiple samples from the data source.
     % Step 2a: Ordinary least squares (without any regularization)
 
     for j=1:length(nTrainArray)
-
+        tic
         betaOLS{j} = sampleTrainX(1:nTrainArray(j),:)\sampleTrainY(1:nTrainArray(j));
         %figure(1); plot(betaOLS,betaTrue,'.'); title(['norm(betaOLS-betaTrue): ' num2str(norm(betaOLS-betaTrue,2))])
         metricsOLS{j} = metric_of_success(sampleTestY,sampleTestX*betaOLS{j},size(sampleTestX,2),'Test','Ridge',0);
+        timeOLS(i,j) = toc;
     end
 
 
     % Step 2b: Ridge regression without side knowledge (with ell_2 regularization)
-
+    if matlabpool('size') > 0, matlabpool close, end
+    matlabpool('open', 'local', 4)
+    
+    
     % The following are the parameter settings we will use for this section.
-
+    %tic %For console logging.
     knowledgeNone = get_knowledge('None',sampleKnowledgeX,sampleKnowledgeY,betaTrue);
-    if(matlabpool('size') == 0) matlabpool; end
+    timeBaselineObj = Par(length(nTrainArray));%A Par object to log iteration time.
     parfor j=1:length(nTrainArray)
-        fprintf('Without knowledge: CV for sample size index j = %d of %d.\n', j, length(nTrainArray));
+        Par.tic;
+        fprintf('Run %d of %d: Without knowledge: CV for sample size index j = %d of %d.\n', i, nRuns, j, length(nTrainArray));
         [betaBaseline{j}, bestBaselineCoeff{j}, cvBaselineMatrix{j}] = ...
             select_model_using_cv('Ridge', coeffRange, nFolds, nRepeats, sampleTrainX(1:nTrainArray(j),:), sampleTrainY(1:nTrainArray(j)), knowledgeNone);
         %figure(2); plot(betaBaseline,betaTrue,'.'); title(['norm(betaBaseline-betaTrue): ' num2str(norm(betaBaseline-betaTrue,2))])
         metricsBaseline{j} = metric_of_success(sampleTestY,sampleTestX*betaBaseline{j},size(sampleTestX,2),'Test','Ridge',0);
+        timeBaselineObj(j) = Par.toc;
     end
-    toc
-
-            
+    %toc
+    stop(timeBaselineObj);
+    for j=1:length(nTrainArray)
+        timeBaseline(i,j) = timeBaselineObj(j).ItStop - timeBaselineObj(j).ItStart;
+    end
+    clear timeBaselineObj;
+    
     % Step 2c: Ridge regression with Linear side knowledge (with ell_2 regularization)
 
     knowledgeLinear = get_knowledge('Linear',sampleKnowledgeX,sampleKnowledgeY,betaTrue);
-
+    timeLinearObj = Par(length(nTrainArray));%A Par object to log iteration time.
     parfor j=1:length(nTrainArray)
-        fprintf('With Linear knowledge: CV for sample size index j = %d of %d.\n', j, length(nTrainArray));
+        Par.tic;
+        fprintf('Run %d of %d: With Linear knowledge: CV for sample size index j = %d of %d.\n', i, nRuns, j, length(nTrainArray));
         [betaLinear{j}, bestLinearCoeff{j}, cvLinearMatrix{j}] = ...
             select_model_using_cv('Ridge', coeffRange, nFolds, nRepeats, sampleTrainX(1:nTrainArray(j),:), sampleTrainY(1:nTrainArray(j)), knowledgeLinear);
         %figure(3); plot(betaLinear,betaTrue,'.'); title(['norm(betaLinear-betaTrue): ' num2str(norm(betaLinear-betaTrue,2))])
         metricsLinear{j} = metric_of_success(sampleTestY,sampleTestX*betaLinear{j},size(sampleTestX,2),'Test','Ridge',0);
+        timeLinearObj(j) = Par.toc;
     end
-    toc
+    %toc
+    stop(timeLinearObj);
+    for j=1:length(nTrainArray)
+        timeLinear(i,j) = timeLinearObj(j).ItStop - timeLinearObj(j).ItStart;
+    end
     
     % Step 2d: Ridge regression with Quadratic side knowledge (with ell_2 regularization)
 
     knowledgeQuadratic = get_knowledge('Quadratic',sampleKnowledgeX,sampleKnowledgeY,betaTrue);
-
+    timeQuadraticObj = Par(length(nTrainArray));%A Par object to log iteration time.
     parfor j=1:length(nTrainArray)
-        fprintf('With Quadratic knowledge: CV for sample size index j = %d of %d.\n', j, length(nTrainArray));
+        Par.tic;
+        fprintf('Run %d of %d: With Quadratic knowledge: CV for sample size index j = %d of %d.\n', i, nRuns, j, length(nTrainArray));
         [betaQuadratic{j}, bestQuadraticCoeff{j}, cvQuadraticMatrix{j}] = ...
             select_model_using_cv('Ridge', coeffRange, nFolds, nRepeats, sampleTrainX(1:nTrainArray(j),:), sampleTrainY(1:nTrainArray(j)), knowledgeQuadratic);
         %figure(3); plot(betaQuadratic,betaTrue,'.'); title(['norm(betaQuadratic-betaTrue): ' num2str(norm(betaQuadratic-betaTrue,2))])
         metricsQuadratic{j} = metric_of_success(sampleTestY,sampleTestX*betaQuadratic{j},size(sampleTestX,2),'Test','Ridge',0);
+        timeQuadraticObj(j) = Par.toc;
     end
-    toc
+    %toc
+    stop(timeQuadraticObj);
+    for j=1:length(nTrainArray)
+        timeQuadratic(i,j) = timeQuadraticObj(j).ItStop - timeQuadraticObj(j).ItStart;
+    end
     
     
     % Step 2e: Ridge regression with Conic side knowledge (with ell_2 regularization)
 
     knowledgeConic = get_knowledge('Conic',sampleKnowledgeX,sampleKnowledgeY,betaTrue);
-
+    timeConicObj = Par(length(nTrainArray));%A Par object to log iteration time.
     parfor j=1:length(nTrainArray)
-        fprintf('With Conic knowledge: CV for sample size index j = %d of %d.\n', j, length(nTrainArray));
+        Par.tic;
+        fprintf('Run %d of %d: With Conic knowledge: CV for sample size index j = %d of %d.\n', i, nRuns, j, length(nTrainArray));
         [betaConic{j}, bestConicCoeff{j}, cvConicMatrix{j}] = ...
             select_model_using_cv('Ridge', coeffRange, nFolds, nRepeats, sampleTrainX(1:nTrainArray(j),:), sampleTrainY(1:nTrainArray(j)), knowledgeConic);
         %figure(3); plot(betaConic,betaTrue,'.'); title(['norm(betaConic-betaTrue): ' num2str(norm(betaConic-betaTrue,2))])
         metricsConic{j} = metric_of_success(sampleTestY,sampleTestX*betaConic{j},size(sampleTestX,2),'Test','Ridge',0);
+        timeConicObj(j) = Par.toc;
     end
-    toc
+    %toc
+    stop(timeConicObj);
+    for j=1:length(nTrainArray)
+        timeConic(i,j) = timeConicObj(j).ItStop - timeConicObj(j).ItStart;
+    end
+    matlabpool close;
+
     
     
     % Collecting performance statistics
